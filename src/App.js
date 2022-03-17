a76a92778a1ed434b53b96a22072868532f69a43
mfkfawas <mfkfawas@gmail.com>
2022-03-17 16:45:38 +0530

    cofigured routes & pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+// import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route
+            path='/forgot-password'
+            element={<ForgotPassword />}
+          />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+        </Routes>
+      </Router>
+      {/* Navbar */}
     </>
   );
 }
